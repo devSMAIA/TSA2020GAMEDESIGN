748e44234b6a339a3eb7704944607cd3bf2bf280
G0ldenPh3onix <60448742+G0ldenPh3onix@users.noreply.github.com>
2020-02-05 13:23:49 -0700

    Add files via upload

---
+++ AIMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMovement : MonoBehaviour
+{
+    public Transform Player;
+    [SerializeField] private float MovementSpeed = 4f;
+    [SerializeField] private float MaxDist = 10f;
+    [SerializeField] private float MinDist = 5f;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.LookAt(Player);
+
+        if (Vector3.Distance(transform.position, Player.position) >= MinDist)
+        {
+            transform.position += transform.forward * MovementSpeed * Time.deltaTime;
+        }
+
+        if(Vector3.Distance(transform.position, Player.position) <= MaxDist)
+        {
+            // Input Firing Script here
+        }
+    }
+}
