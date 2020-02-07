3b09720334dbb853d96cfc06747951bd119869c6
G0ldenPh3onix <60448742+G0ldenPh3onix@users.noreply.github.com>
2020-02-07 13:45:48 -0700

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector3 movement;
+    public CharacterController controller;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        movement = new Vector3(Input.GetAxis("Horizontal"), -.2f, Input.GetAxis("Vertical"));
+
+        controller.Move(movement);
+    }
+}
